34fa9548b2778bd8b5e114cd39810cb27c8dee44
Shekinah Cage <scage@catalyte.io>
2026-07-21 12:41:56 -0500

    created second endpoint that accepts a name path parameter.

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "Hello, World!"}
+
+@app.get("/hello/{name}")
+def hello_name(name:str):
+    return {"message": f"Hello, {name}!"}
