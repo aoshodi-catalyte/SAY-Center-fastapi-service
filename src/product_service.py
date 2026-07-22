e2bc9d464c6ec97ed46b8aa2d65aad2d37a69e1d
Shekinah Cage <scage@catalyte.io>
2026-07-22 15:25:25 -0500

    defined products endpoint.

---
+++ src/product_service.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from product.product_repository import ProductRepository
+from product.product_model import Product
+
+app = FastAPI()
+product_repo = ProductRepository()
+
+@app.get("/")
+def home_page():
+    return {"message": "Hello!"}
+
+@app.post("/products")
+def post_product(product: Product):
+    # new_product = Product(name, unit, cost_per_unit, price_per_unit, quantity_in_stock)
+    product_repo.add_product(product)
+    return {"message": "New Product added successfully!", "product": product}
