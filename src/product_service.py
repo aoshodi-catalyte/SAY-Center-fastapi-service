20da29ae88c25ae72708eda45f7ce44c73886fd4
Adeyemi Oshodi <aoshodi@catalyte.io>
2026-07-23 12:15:47 -0500

    added product search function

--- src/product_service.py
+++ src/product_service.py
@@ -18,3 +18,16 @@ def post_product(product: Product):
 @app.get("/products")
 def get_products():
     return product_repo.get_all_products()
+
+@app.get("/products/search")
+def search_products(name: str, unit: str | None = None):
+    products = product_repo.get_all_products()
+
+    matching_products = []
+
+    for product in products:
+        if product.name == name:
+            if unit is None or product.unit == unit:
+                matching_products.append(product)
+
+    return matching_products
