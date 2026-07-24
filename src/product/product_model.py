b591325e1d4fa401e2125f533d65a33f860b87ff
Arlette Diaz <adiaz@catalyte.io>
2026-07-24 10:22:26 -0500

    updated product model to have an id

--- src/product/product_model.py
+++ src/product/product_model.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, Field
+from typing import Optional
+
 
 class Product(BaseModel):
-    name: str 
+    id: Optional[int] = None
+    name: str
     unit: str
     cost_per_unit: float = Field(ge=0)
     price_per_unit: float = Field(ge=0)
     quantity_in_stock: float = Field(ge=0)
-
