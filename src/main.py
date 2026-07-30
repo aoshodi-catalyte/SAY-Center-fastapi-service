ff96a0e48083d45f66aa9ace0422497d6963d99a
Arlette Diaz <adiaz@catalyte.io>
2026-07-30 10:50:42 -0500

    Updated main file imports and added routers

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from product_router import router as product_router
+from category_router import router as category_router
+
+app = FastAPI()
+
+app.include_router(product_router)
+app.include_router(category_router)
